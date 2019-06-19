0532e7b0d0ae36d50fe480123118d4b77996757d
Starla Yang <koola025@gmail.com>
2019-06-19 16:39:04 +0800

    done plotting

--- hw5_mqtt/message.py
+++ hw5_mqtt/message.py
@@ -47,6 +47,42 @@ s = serial.Serial(serdev, 9600)
 # print("Exit AT mode.")
 
 # print(char.decode())
+
+import paho.mqtt.client as paho
+import time
+mqttc = paho.Client()
+
+# Settings for connection
+host = "localhost"
+topic= "Mbed"
+port = 1883
+
+# Callbacks
+def on_connect(self, mosq, obj, rc):
+    print("Connected rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n");
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed OK")
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+    print("Unsubscribed OK")
+
+# Set callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe
+
+# Connect and subscribe
+print("Connecting to " + host + "/" + topic)
+mqttc.connect(host, port=1883, keepalive=60)
+mqttc.subscribe(topic, 0)
+    
+
+
 first = True
 while True:
     if first:
@@ -55,6 +91,10 @@ while True:
     else:
         line = s.read(13)
     print('Get:', line.decode())
+
+    mesg = line.decode()
+    mqttc.publish(topic, mesg)
+    print(mesg)
     time.sleep(1)
     # s.write(fan.encode())
     # line = s.read(20)
